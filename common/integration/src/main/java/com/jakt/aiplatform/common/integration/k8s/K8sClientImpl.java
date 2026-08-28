3a9a6e4348017ca3b873689e3eae3b7ee5dd3f65
jakt <602979469@qq.com>
2026-08-29 01:46:11 +0800

    增加测试环境开关

--- common/integration/src/main/java/com/jakt/aiplatform/common/integration/k8s/K8sClientImpl.java
+++ common/integration/src/main/java/com/jakt/aiplatform/common/integration/k8s/K8sClientImpl.java
@@ -18,6 +18,7 @@ import io.fabric8.kubernetes.client.*;
 import io.fabric8.kubernetes.client.dsl.LogWatch;
 import io.fabric8.kubernetes.client.dsl.PodResource;
 import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.io.ByteArrayOutputStream;
@@ -37,6 +38,9 @@ import java.util.Objects;
 @Component
 public class K8sClientImpl implements K8sClient, DisposableBean {
 
+    @Value("${k8s.test:false}")
+    private boolean testEnable;
+
     public static final String TOKEN = "eyJhbGciOiJSUzI1NiIsImtpZCI6IndEdmR6Z1F4cnA5X1dtY3VfaWRFZEhFdmE0di1lSEdpc3hWN18zR0dnSU0ifQ.eyJpc3MiOiJrdWJlcm5ldGVzL3NlcnZpY2VhY2NvdW50Iiwia3ViZXJuZXRlcy5pby9zZXJ2aWNlYWNjb3VudC9uYW1lc3BhY2UiOiJkZWZhdWx0Iiwia3ViZXJuZXRlcy5pby9zZXJ2aWNlYWNjb3VudC9zZWNyZXQubmFtZSI6ImphdmEtY2xpZW50LXNlY3JldCIsImt1YmVybmV0ZXMuaW8vc2VydmljZWFjY291bnQvc2VydmljZS1hY2NvdW50Lm5hbWUiOiJqYXZhLWNsaWVudCIsImt1YmVybmV0ZXMuaW8vc2VydmljZWFjY291bnQvc2VydmljZS1hY2NvdW50LnVpZCI6ImU4YjRlMWFlLTllZGUtNDUxYS05OTEyLWE1YWNiMWQ3Y2QwMiIsInN1YiI6InN5c3RlbTpzZXJ2aWNlYWNjb3VudDpkZWZhdWx0OmphdmEtY2xpZW50In0.xw3JaBdRPRpFygQHUHJY4-274vBKCk45bK-RarbExMIZ66AzGs94OsnZrP3yF8NKzeW7wS4BGEaLYJyWrdPaNIBGqe8K08wfcEqNbRc8tzDwsO9fiw_xNmZ7ApYjwRHChmpBa-hNwsQHx7_Rm4QrUzLRZNiPWllF9F6lP-jzboBbhKEZm6-u0sMC_1x2sKs0eYLT2t4KOgq0rpEOcDQIafr2L8nTizaQU9dUTOQh1oHvl9Xi-C27gMVY6KhwOh_rHQGjEI5bmqQygqgdpNwuIQx1BhifqFtpEsToqzF2Y-Xa3_nOoy-deP_bQikMwnd4Y2Kl3EzpXWqxe4BWL8l-Aw";
 
     /**
@@ -45,15 +49,19 @@ public class K8sClientImpl implements K8sClient, DisposableBean {
     private final KubernetesClient kubernetesClient;
 
     public K8sClientImpl() {
-        Config config = new ConfigBuilder()
-                .withMasterUrl("https://192.168.3.131:6443")
-                .withOauthToken(TOKEN)  // 使用你的Token
-                .withTrustCerts(true)   // 跳过SSL证书验证（测试环境）
-                .build();
+        if (testEnable){
+            Config config = new ConfigBuilder()
+                    .withMasterUrl("https://192.168.3.131:6443")
+                    .withOauthToken(TOKEN)  // 使用你的Token
+                    .withTrustCerts(true)   // 跳过SSL证书验证（测试环境）
+                    .build();
 
-        this.kubernetesClient = new KubernetesClientBuilder()
-                .withConfig(config)
-                .build();
+            this.kubernetesClient = new KubernetesClientBuilder()
+                    .withConfig(config)
+                    .build();
+            return;
+        }
+       this.kubernetesClient = new KubernetesClientBuilder().build();
     }
 
     @Override
